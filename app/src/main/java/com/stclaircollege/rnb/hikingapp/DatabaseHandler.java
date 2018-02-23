e5337b24277bff003a8425fa2eec70b3ab0a3ce2
Royce Nguyen <Hoangtu.Nguyen11@stclairconnect.ca>
2018-02-23 02:42:21 -0500

    Some database work

--- app/src/main/java/com/stclaircollege/rnb/hikingapp/DatabaseHandler.java
+++ app/src/main/java/com/stclaircollege/rnb/hikingapp/DatabaseHandler.java
@@ -1,8 +1,53 @@
 package com.stclaircollege.rnb.hikingapp;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 /**
  * Created by Royce on 1/30/2018.
  */
 
-public class DatabaseHandler {
+public class DatabaseHandler extends SQLiteOpenHelper
+{
+    private static final int DATABASE_VERSION = 1;
+
+    /**
+     * create the name of the database
+     */
+    private static final String DATABASE_NAME = "hikingapp";
+
+    /**
+     * Create the names of all the tables
+     */
+    private static final String TABLE_TRIP = "trip";
+    private static final String TABLE_HIKE = "hike";
+    private static final String TABLE_PARTICIPANT = "participant";
+
+    /**
+     * Common column names
+     */
+    private static final String COLUMN_ID = "id";
+    /**
+     * Trip Table Column Names
+     */
+    private static final String COLUMN_LOCATION = "location";
+    private static final String COLUMN_STARTDATE = "startdate";
+    private static final String COLUMN_ENDDATE = "enddate";
+    private static final String COLUMN_NOOFDAYS= "attackdamage";
+    private static final String COLUMN_ATTACKSPEED = "attackspeed";
+    private static final String COLUMN_MAGICDAMAGE = "magicdamage";
+    private static final String COLUMN_CRIT = "crit";
+    private static final String COLUMN_CRITDAMAGE = "critdamage";
+    private static final String COLUMN_HEALTH = "health";
+    private static final String COLUMN_ARMOR = "ARMOR";
+    private static final String COLUMN_MAGICRESIST = "magicresist";
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
