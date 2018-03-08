05f9e031b74cf67418420ce8474cda133baa0e7e
Royce Nguyen <Hoangtu.Nguyen11@stclairconnect.ca>
2018-03-08 14:23:34 -0500

    Added additional object classes

---
+++ app/src/main/java/com/stclaircollege/rnb/hikingapp/Location.java
@@ -0,0 +1,67 @@
+package com.stclaircollege.rnb.hikingapp;
+
+/**
+ * Created by Royce on 3/8/2018.
+ */
+
+public class Location {
+    private int id;
+    private String location;
+    private String city;
+    private String country;
+    private String type;
+
+
+
+    public Location(){
+
+    }
+
+    public Location(int id,String location,String city,String country,String type){
+        this.id = id;
+        this.location = location;
+        this.city = city;
+        this.country = country;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
