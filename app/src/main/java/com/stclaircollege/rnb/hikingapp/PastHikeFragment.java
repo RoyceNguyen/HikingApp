2d0c27c70a3725acfda5dcc933d9b25890ad29a6
Blaze Blackbird <blaze.blackbird01@stclairconnect.ca>
2018-04-08 22:15:41 -0400

    Started coding in pasthikefragment to implement our listview to show the hikes

--- app/src/main/java/com/stclaircollege/rnb/hikingapp/PastHikeFragment.java
+++ app/src/main/java/com/stclaircollege/rnb/hikingapp/PastHikeFragment.java
@@ -4,9 +4,16 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import static com.stclaircollege.rnb.hikingapp.MainActivity.fab;
 
 
 /**
@@ -26,6 +33,8 @@ public class PastHikeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    ListView list;
 
     private OnFragmentInteractionListener mListener;
 
@@ -60,11 +69,37 @@ public class PastHikeFragment extends Fragment {
         }
     }
 
+    //Creating fragment manager
+    FragmentManager fm;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_past_hike, container, false);
+        View view = inflater.inflate(R.layout.fragment_past_hike, container, false);
+
+        fm = getActivity().getSupportFragmentManager();
+        //Had to add this fab.show in order to create a trip
+        fab.show();
+        fab.setImageResource(R.drawable.ic_add_box_black_24dp);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.addToBackStack(null);
+                ft.replace(R.id.content_main, new AddTripFragment());
+                ft.commit();
+            }
+        });
+
+        list = (ListView) view.findViewById(R.id.tripslist);
+        DatabaseHandler db = new DatabaseHandler(getContext());
+        final ArrayList<Trip> tripslist = db.getAllTrips();
+        db.closeDB();
+
+
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
